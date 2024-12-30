bac2de6a5bf447172b6ebfdc2b47c881f9647693
hadi <hadi@hadi.com>
2024-12-30 20:36:08 +0330

    video-6

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -1,29 +1,39 @@
-import { useParams } from "react-router-dom"
-import Container from "../../components/container/Container"
+import { useParams } from "react-router-dom";
+import Container from "../../components/container/Container";
 
-function Product(){
-    const params = useParams()
-    return (
-            <div>
-              <Container>
+function Product() {
+  const params = useParams();
+  return (
+    <div>
+      <Container>
+        <h1>ID = {params.id}</h1>
+        <div className=" shadow mt-4 h-96 grid grid-cols-12 bg-yellow-200">
+          <div className="col-span-2 m-5 bg-red-400">
+            <img
+              className="rounded "
+              src="https://m.media-amazon.com/images/I/618pgJjYR3L.__AC_SX300_SY300_QL70_ML2_.jpg"
+            />
+             <div className="m-4">
+            <button>Add to cart</button>
+          </div>
+          </div>
+         
+          <div className="col-span-10 m-1  bg-slate-400">
+            <h1>Title: Galaxy</h1>
+            <p>Price: 25$</p>
+            <p>
+              Description: Meet the Galaxy S23 and S23+ phone with Galaxy AI.
+              Share the epic with crisp Nightography on the selfie camera,
+              powerful processor and a display that.
+            </p>
+          </div>
+        </div>
 
-                <div className="h-96 shadow mt-4">
-
-                <div className="bg-red-400">
-                    <img src="https://m.media-amazon.com/images/I/618pgJjYR3L.__AC_SX300_SY300_QL70_ML2_.jpg"/>
-                </div>
-
-                </div>
-                <div className="bg-orange-500">
-
-                </div>
-                </Container>
-
-
-
-
-            </div>
-
-    )
+        <div></div>
+      </Container>
+    </div>
+  );
 }
-export default Product
+export default Product;
+
+// video6 9min
